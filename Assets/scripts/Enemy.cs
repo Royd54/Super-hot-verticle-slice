9adef45ce649cdd3e0134bd966fbb42cb0a7254d
26689 <tasso.mevissen@gmail.com>
2020-01-21 19:33:57 +0100

    Tasso's merge upload
    stuur mijn files naar roy's github

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    Animator anim;
+
+    public float damageAmount = 1f;
+
+    [SerializeField]
+    float attackTime = 2f;
+
+     void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+
+     void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+        
+        if(distance > 0.5)
+        {
+            agent.updatePosition = true;
+            agent.SetDestination(target.position);
+            anim.SetBool("Idle", false);
+            anim.SetBool("Attack", false);
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            agent.updatePosition = false;
+            anim.SetBool("Idle", false);
+            anim.SetBool("Attack", true);
+            anim.SetBool("walking", true);
+            StartCoroutine(AttackTime());
+        }
+    }
+
+   IEnumerator AttackTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        PlayerHealth.singleton.DamagePlayer(damageAmount);
+        yield return new WaitForSeconds(attackTime);
+    }
+
+
+    // transform. forward gebruiken met time. delta time
+    //navmesh gebruiken
+    //navmes op component zetten
+    //chek er op zetten over distance 
+    //bullet kan naar emty toe gaan in speler
+}
+
+
+
+
+
---
+++ Assets/scripts/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    GameObject target;
+    public float fireRate = 1f;
+    private float fireCountdown = 5f;
+    private float speed = 10f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float DestroyTime = 0.1f;
+
+    void Update()
+    {
+        if (fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+       var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    
+
+        Destroy(bullet, DestroyTime);
+    }
+
+}
